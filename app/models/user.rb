40cba7d8c411de96498e2fff1c2075d48f269d50
masashihamano <masashi.hamano@gmail.com>
2018-10-25 00:07:59 +0900

    finish 4 chapter

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   # img_name_uploaderファイルと連携
   mount_uploader :img_name, ImgNameUploader
+
+  enum sex: { 男: 0, 女: 1 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
 
+  resources :users, only: [:index, :show]
+
+  # resources :users, only: %i(index show)
+
 end
