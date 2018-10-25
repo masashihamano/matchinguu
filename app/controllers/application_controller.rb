6b84dea8af4f77f286b3f4a55e122cf162ccf00b
masashihamano <masashi.hamano@gmail.com>
2018-10-25 12:00:22 +0900

    bundle

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+
+protected
+
+ def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :self_introduction, :sex, :img_name])
+   devise_parameter_sanitizer.permit(:account_update, keys: [:name, :self_introduction, :sex, :img_name])
+ end
+ # def configure_permitted_parameters
+ #   devise_parameter_sanitizer.permit(:sign_up, keys: %i(name self_introduction sex img_name))
+ #   # この行を追加する
+ #   devise_parameter_sanitizer.permit(:account_update, keys: %i(name self_introduction sex img_name))
+ # end
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root 'top#index'
+
+  resources :users, only: [:index, :show]
+
+  # resources :users, only: %i(index show)
+
+end
